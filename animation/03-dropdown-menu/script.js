b5cfaa6a7f6da4ae87a4c8222713e3ee2814b282
Aarón <aaronaithorhl2003@gmail.com>
2024-09-23 17:42:02 +0300

    Cambios a ser confirmados: 	modificados:     animation/01-button-hover/style.css 	modificados:     animation/02-pop-up/style.css 	modificados:     animation/03-dropdown-menu/script.js 	modificados:     animation/03-dropdown-menu/style.css 	modificados:     flex/01-flex-center/style.css 	modificados:     flex/02-flex-header/index.html 	modificados:     flex/02-flex-header/style.css 	modificados:     flex/03-flex-header-2/index.html 	modificados:     flex/03-flex-header-2/style.css 	modificados:     flex/04-flex-information/index.html 	modificados:     flex/04-flex-information/style.css 	modificados:     flex/05-flex-modal/index.html 	modificados:     flex/05-flex-modal/style.css

--- animation/03-dropdown-menu/script.js
+++ animation/03-dropdown-menu/script.js
@@ -4,7 +4,16 @@ const dropdownMenu = document.querySelector(".dropdown-menu");
 
 menuTitle.addEventListener("click", (e) => {
   if (e.target === e.currentTarget) {
-    dropdownMenu.classList.toggle("visible");
+    dropdownMenu.classList.toggle("visible")
+    menuTitle.animate(
+      [{transform: 'scaleY(1)'},
+       {transform: 'scaleY(1)'},
+       {transform: 'scaleY(0.8)'},
+       {transform: 'scaleY(1)'}],
+      {
+        duration:500
+      }
+    )
   }  
 })
 
